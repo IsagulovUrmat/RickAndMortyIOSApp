5d366836d7fab0df09b4c02255a486b1d7a743a3
isagulovurmat <falone641234@gmail.com>
2025-03-19 17:56:33 +0600

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by sunflow on 19/3/25.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
