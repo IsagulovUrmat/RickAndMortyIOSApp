49edc32fd1a6112f7a90c8669d5050852cbc64e3
isagulovurmat <falone641234@gmail.com>
2025-03-20 18:58:11 +0600

    added detailed view for characters

---
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  RMCharacterDetailViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by sunflow on 20/3/25.
+//
+
+import Foundation
+
+final class RMCharacterDetailViewViewModel {
+    
+    private let character: RMCharacter
+    
+    init(character: RMCharacter) {
+        self.character = character
+    }
+    
+    public var title: String {
+        character.name.uppercased()
+    }
+}
---
+++ RickAndMorty/Views/RMCharacterDetailView.swift
@@ -0,0 +1,13 @@
+//
+//  RMCharacterDetailView.swift
+//  RickAndMorty
+//
+//  Created by sunflow on 20/3/25.
+//
+
+import UIKit
+
+final class RMCharacterDetailView: UIView {
+
+
+}
