055d000c38b1a05b74f962bdb29ef562e69f550f
isagulovurmat <falone641234@gmail.com>
2025-03-23 18:43:44 +0600

    Build out character detailed screen

--- RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
+++ RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
@@ -11,10 +11,38 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     
     static let cellIdentifier = "RMCharacterEpisodeCollectionViewCell"
     
+    private let seasonLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .regular)
+        return label
+    }()
+    
+    private let airDateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .light)
+        return label
+    }()
+    
+    
+    // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .systemBlue
+        contentView.backgroundColor = .tertiarySystemBackground
         contentView.layer.cornerRadius = 8
+        contentView.layer.borderWidth = 2
+        contentView.layer.borderColor = UIColor.systemGray.cgColor
+        
+        contentView.addSubviews(seasonLabel, nameLabel, airDateLabel)
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -22,18 +50,36 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            seasonLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            seasonLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            seasonLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            seasonLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.3),
+            
+            nameLabel.topAnchor.constraint(equalTo: seasonLabel.bottomAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            nameLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.3),
+            
+            airDateLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor),
+            airDateLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            airDateLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            airDateLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.3),
+        ])
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        nameLabel.text = nil
+        seasonLabel.text = nil
+        airDateLabel.text = nil
     }
     
     public func configure(with viewModel: RMCharacterEpisodeCollectionViewCellViewModel) {
-        viewModel.registerForData { data in
-            print(data.name)
-            print(data.air_date)
-            print(data.episode)
+        viewModel.registerForData { [weak self] data in
+            self?.nameLabel.text = data.name
+            self?.seasonLabel.text = "Episode "+data.episode
+            self?.airDateLabel.text = "Aired on "+data.air_date
         }
         viewModel.fetchEpisode()
     }
