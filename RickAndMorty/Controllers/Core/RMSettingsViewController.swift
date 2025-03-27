d5604d00663fc49392c7606ca598dd5675843418
isagulovurmat <falone641234@gmail.com>
2025-03-27 18:10:15 +0600

    Build up SwiftUI Settings view

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -5,22 +5,38 @@
 //  Created by sunflow on 19/3/25.
 //
 
+import SwiftUI
 import UIKit
 
 /// Controller to show various app options and settings
 final class RMSettingsViewController: UIViewController {
 
-    private let viewModel = RMSettingsViewViewModel(cellViwModels: RMSettingsOption.allCases.compactMap({
-        return RMSettingsCellViewModel(type: $0)
-    }))
     
-  
+    private let settingsSwiftUIController = UIHostingController(rootView: RMSettingsView(viewModel: RMSettingsViewViewModel(cellViwModels: RMSettingsOption.allCases.compactMap({
+        return RMSettingsCellViewModel(type: $0)
+    }))))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         title = "Settings"
+        addSwiftUIController()
+    }
+    
+    private func addSwiftUIController() {
+        addChild(settingsSwiftUIController)
+        settingsSwiftUIController.didMove(toParent: self)
         
+        view.addSubview(settingsSwiftUIController.view)
+        settingsSwiftUIController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            settingsSwiftUIController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            settingsSwiftUIController.view.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            settingsSwiftUIController.view.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
 
 }
---
+++ RickAndMorty/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,44 @@
+//
+//  RMSettingsView.swift
+//  RickAndMorty
+//
+//  Created by sunflow on 27/3/25.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    
+    let viewModel: RMSettingsViewViewModel
+    
+    init(viewModel: RMSettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        List(viewModel.cellViwModels) { viewModel in
+            HStack {
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(Color.white)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .padding(8)
+                        .background(Color(viewModel.iconContainerColor))
+                        .cornerRadius(6)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 10)
+            }
+            .padding(.bottom, 3)
+        }
+    }
+}
+
+#Preview {
+    RMSettingsView(viewModel: .init(cellViwModels: RMSettingsOption.allCases.compactMap({
+        return RMSettingsCellViewModel(type: $0)
+    })))
+}
